435322544d75140798fdf23c30d1d6ce6525d805
lioneie <634930778@qq.com>
2019-03-17 22:37:20 +0800

    Tx multicast data in tdma session scheduler

--- internal/tdma_session/scheduler.go
+++ internal/tdma_session/scheduler.go
@@ -8,8 +8,20 @@ import (
 	log "github.com/sirupsen/logrus"
 
 	"github.com/lioneie/lora-tdma-server/internal/config"
+	"github.com/lioneie/lora-tdma-server/internal/multicast"
 	"github.com/lioneie/lora-tdma-server/internal/storage"
 )
+
+var errAbort = errors.New("")
+
+type tdmaSessionContext struct {
+	item storage.TdmaSessionItem
+}
+
+var tdmaSessionTasks = []func(*tdmaSessionContext) error{
+	txMulticastData,
+	calcNextTime,
+}
 
 func TdmaSessionSchedulerLoop() {
 	for {
@@ -28,15 +40,44 @@ func ScheduleTdmaSession() error {
 	}
 
 	for _, item := range tdmaSessionItems {
-		//TODO
-		log.Info(item)
-		calcNextTime(item)
+		log.Debug(item)
+		_ = handleTdmaSessionItem(item)
 	}
 
 	return nil
 }
 
-func calcNextTime(item storage.TdmaSessionItem) {
+func handleTdmaSessionItem(item storage.TdmaSessionItem) error {
+	ctx := tdmaSessionContext{
+		item: item,
+	}
+
+	for _, t := range tdmaSessionTasks {
+		if err := t(&ctx); err != nil {
+			if err == errAbort {
+				return nil
+			}
+			return err
+		}
+	}
+
+	return nil
+}
+
+func txMulticastData(ctx *tdmaSessionContext) error {
+	var mcData []byte = []byte{0xa, 0xb, 0xc, 0xd, 0xe}
+	MulticastGroupId := "4a21c7f8-4111-4e46-97c9-2986ca60bac5" //TODO
+	fcnt, err := multicast.Enqueue(MulticastGroupId, 5, mcData)
+	if err == nil {
+		log.Info("send multicaset success, fcnt: ", fcnt)
+	} else {
+		log.Error("send multicast err:", err)
+	}
+	return nil
+}
+
+func calcNextTime(ctx *tdmaSessionContext) error {
+	item := ctx.item
 	var tdmaJoinItem storage.TdmaJoinItem
 	err := storage.Transaction(config.C.PostgreSQL.DB, func(tx sqlx.Ext) error {
 		var err error
@@ -44,9 +85,10 @@ func calcNextTime(item storage.TdmaSessionItem) {
 		return err
 	})
 	if err != nil {
-		return
+		return err
 	}
 	log.Debug(tdmaJoinItem.TxCycle)
 	item.Time = time.Now().Add(time.Duration(tdmaJoinItem.TxCycle) * time.Millisecond)
 	_ = storage.CreateTdmaSessionItemCache(config.C.Redis.Pool, item)
+	return nil
 }
